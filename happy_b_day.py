aa39b75be5d89ccb0f961596c15191a7c44815a5
MarshallMor <memorris1@shockers.wichita.edu>
2025-03-14 22:01:15 -0500

    Update happy_b_day.py

--- happy_b_day.py
+++ happy_b_day.py
@@ -1,3 +1,6 @@
 import streamlit as st
+
+st.title("Happy Birthday Cel")
 VIDEO_URL = "https://www.youtube.com/watch?v=J_y9MqOYyiQ"
 st.video(VIDEO_URL)
+st.caption("This is a quick webpage I made to celebrate your birthday. The project i was trying to make will take a little longer that initaly estimated so here is a countdown to the actual present")
